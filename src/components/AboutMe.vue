c78c04dc45ac64acea5df9f63f76b3a840472b9d
ColeWalker <colegrwalker@gmail.com>
2020-06-18 20:40:17 -0400

    updated about me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,10 +4,20 @@
         <div class="professional-experience-wrapper-inner">
             <h2>About Me</h2>
             <p>
-               I'm a Software Engineer based out of New Jersey. I currently work as a Front-End Developer for Hudson Integrated. I'm expecting to graduate from William Paterson University with a Bachelor of Science in Computer Science in May, 2020. I'm skilled in both front-end and back-end development, I have extensive experience with Node.js, React, jQuery, SCSS, and web accessibility. 
+               I'm a Software Engineer based out of New Jersey. I currently work as a Front-End Developer for Hudson Integrated. 
+               I graduated Cum Laude from William Paterson University with a Bachelor's of Computer Science in May 2020. 
+               I'm skilled in both front-end and back-end development, I have extensive experience with Node.js, React, jQuery, SCSS, 
+               and web accessibility, and I've dabbled in Vue, (this site was built with Vue and Gridsome) AngularJS, Python, Go, Java, C++,
+               SQL, and the list goes on. I'm a tinkerer, so I've played around with more technologies than would be reasonable to list.
+               I live to learn and love to explore new and interesting things.
             </p>
             <p>
-                I'm looking for roles which will allow me to interact with both ends of the tech stack, and have me frequently interacting with the logical side of things - I love flowcharts. 
+                I'm looking for roles which will allow me to interact with both ends of the tech stack, 
+                and have me frequently solving complex problems with interesting solutions. Nothing feels 
+                more satisfying to me than building applications which required a lot of thought and 
+                effort.
+            </p>
+            <p>
             </p>
             <footer>
                 <img src="../images/technologyrow.png" alt="jQuery, Node.js, React, SCSS" />
